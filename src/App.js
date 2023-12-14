4b0f2d55fd06c24d4e91c0159f398f4335dd1d57
이정윤 <jylee@innogrid.com>
2023-12-14 12:52:33 +0900

    3

--- src/App.js
+++ src/App.js
@@ -2,25 +2,10 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+    console.log(window.ethereum);
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <p>{window}</p>
-                <br/>
-                <p>{window.ethereum}</p>
-            </header>
+            <header className="App-header"></header>
         </div>
     );
 }
