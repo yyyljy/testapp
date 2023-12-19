aaa71bb5ab759d91ddfddeac54520608733826db
이정윤 <jylee@innogrid.com>
2023-12-19 17:04:00 +0900

    ...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-    alert(Object.keys(window));
+    alert(window.LGWallet);
     // alert(JSON.stringify(window));
     return (
         <div className="App">
